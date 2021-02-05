a2e0a71f833810811bdcd21b162f4aa6c58ddf92
Andrei Samuilik <a.samuylik@gmail.com>
2021-02-05 14:13:11 +0300

    build.sh

--- build.sh
+++ build.sh
@@ -5,14 +5,19 @@ mkdir -p build/proto
 GITHUB_PKG=github.com/AgentCoop
 PROTO_ROOT=~/repos/github/peppermint/api/strawman
 PROTO_BUILD=./build/proto
+GEN_OUTPUT=./internal/api
 
 rm -rf $PROTO_BUILD/*
 cp -r $PROTO_ROOT/* $PROTO_BUILD/
 find $PROTO_BUILD -name '*.proto' -exec sed -r -i "s@option go_package = \"(.*)\";@option go_package = \"$GITHUB_PKG/\1\";@g" {} \;
 
-for file in $(find $PROTO_ROOT -name '*.proto' -type f);
+for file in $(find $PROTO_BUILD -name '*.proto' -type f);
 do
   echo Compiling ...$(basename $file)
-  #protoc -I$PROTO_ROOT --go_opt=M=foo --go_out=out --go_opt=paths=source_relative $file
+  protoc -I="$PROTO_BUILD" \
+    --go_opt=paths=source_relative \
+    --go-grpc_opt=paths=source_relative \
+    --go_out=$GEN_OUTPUT \
+    --go-grpc_out=$GEN_OUTPUT $file
 done
 
